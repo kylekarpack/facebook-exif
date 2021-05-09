1412fde67375946e89be47123842554dac5323a2
Kyle Karpack <kylekarpack@gmail.com>
2021-05-08 18:48:09 -0700

    Update to use file times

--- index.go
+++ index.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"path/filepath"
 	"strings"
-
-	exif "github.com/dsoprea/go-exif/v3"
-	jpegstructure "github.com/dsoprea/go-jpeg-image-structure/v2"
-	log "github.com/dsoprea/go-logging"
+	"time"
 )
 
 var exifMap = make(map[string]Photo)
@@ -23,7 +21,9 @@ func main() {
 
 func readFile(filename string) Album {
 	content, err := ioutil.ReadFile(filename)
-	log.PanicIf(err)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	var album Album
 	json.Unmarshal([]byte(content), &album)
@@ -40,54 +40,32 @@ func processAlbum(album Album) {
 }
 
 func processPhoto(path string) {
-	jmp := jpegstructure.NewJpegMediaParser()
+	// get last modified time
+	file, err := os.Stat(path)
 
-	intfc, err := jmp.ParseFile(path)
-	log.PanicIf(err)
-
-	sl := intfc.(*jpegstructure.SegmentList)
-
-	// Update the CameraOwnerName tag.
-
-	rootIb, err := sl.ConstructExifBuilder()
-	log.PanicIf(err)
-
-	ifdPath := "IFD/Exif"
-
-	ifdIb, err := exif.GetOrCreateIbFromRootIb(rootIb, ifdPath)
-	log.PanicIf(err)
-
-	err = ifdIb.SetStandardWithName("CameraOwnerName", "TestOwner")
-	log.PanicIf(err)
-
-	// Update the exif segment.
-
-	err = sl.SetExif(rootIb)
-	log.PanicIf(err)
-
-	b := new(bytes.Buffer)
-
-	err = sl.Write(b)
-	log.PanicIf(err)
-
-	// Validate.
-
-	d := b.Bytes()
-
-	intfc, err = jmp.ParseBytes(d)
-	log.PanicIf(err)
-
-	sl = intfc.(*jpegstructure.SegmentList)
-
-	_, _, exifTags, err := sl.DumpExif()
-	log.PanicIf(err)
-
-	for _, et := range exifTags {
-		if et.IfdPath == "IFD/Exif" && et.TagName == "CameraOwnerName" {
-			fmt.Printf("Value: [%s]\n", et.FormattedFirst)
-			break
-		}
+	if err != nil {
+		fmt.Println(err)
 	}
+
+	modifiedtime := file.ModTime()
+
+	fmt.Println("Last modified time : ", modifiedtime)
+
+	// get current timestamp
+
+	currenttime := time.Now().Local()
+
+	fmt.Println("Current time : ", currenttime.Format("2006-01-02 15:04:05 +0800"))
+
+	// change both atime and mtime to currenttime
+
+	err = os.Chtimes(path, currenttime, currenttime)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Changed the file time information")
 
 }
 
@@ -95,7 +73,9 @@ func getFiles() {
 
 	albums, err := filepath.Glob("./photos_and_videos/albums/**/*.json")
 
-	log.PanicIf(err)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	for _, path := range albums {
 		album := readFile(path)
@@ -106,8 +86,10 @@ func getFiles() {
 func getPhotos() {
 
 	photos, err := filepath.Glob("./photos_and_videos/**/*.jpg")
-	log.PanicIf(err)
 
+	if err != nil {
+		log.Panic(err)
+	}
 	fmt.Println(photos[0])
 	processPhoto(photos[0])
 	// for _, photo := range photos {
